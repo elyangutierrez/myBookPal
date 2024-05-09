722f826f0567e5fa3099acd205448db3485839e6
Elyan Gutierrez <elyann60@gmail.com>
2024-05-08 22:06:59 -0500

    Initial project

---
+++ myBookPal/myBookPal/myBookPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBookPalApp.swift
+//  myBookPal
+//
+//  Created by Elyan Gutierrez on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct myBookPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
